0f3ffaa2fdc94026c617c2bbf68bc95628c13b2c
danielware <waredanielb@gmail.com>
2022-06-10 08:55:37 -0700

    Register first test as passing.

---
+++ README.md
@@ -0,0 +1,9 @@
+Describe: robogersHood()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: robogersHood(5);
+Expected Output: [0, 1, 2, 3, 4, 5]
+
+Test: "If the value of an index contains a "1", it should return a true value"
+code robogersHood(5);
+Expected Output: [0, true, 2, 3, 4, 5]
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//Business Logic
+
+function robogersHood(userInput) {
+  const initialArray = [];
+  for (let i = 0; i <= userInput; i++) {
+    initialArray.push(i);
+    console.log(initialArray);
+  }
+
+
+
+
+
+
+}
+
+//User Logic
+
+$(document).ready(function() {
+  $("form#Quiz").submit(function(event) {
+    
+    const inputAge = parseInt($("#age").val());
+
+  });
+});
